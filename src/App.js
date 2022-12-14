48166c2baa12fb9fd9d18d8fe0329b3a92d34705
Praba <prama1303@gmail.com>
2022-12-15 00:16:53 +0100

    page Home and About added & mark NavLink create automatic active class

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Hallo, PR!</h1>
+      <BrowserRouter>
+        <header>
+          <nav>
+            <h1>PR-Router</h1>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='about'>About</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='about' element={<About />} />
+            {/* /about  / not necessary*/}
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Page not found!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias cupiditate ad nostrum doloribus iste tempora quisquam excepturi repellat, fugit cumque dolore magni possimus inventore neque provident! Sunt, quo eos?</p>
+        </div>
+    );
+};
+
+export default NotFound;
