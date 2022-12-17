ec947267da6e229b99485508f63dff5c3cefa5eb
Praba <prama1303@gmail.com>
2022-12-17 01:09:48 +0100

    Layout & Nested Routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import {
   createRoutesFromElements,
   RouterProvider
 } from 'react-router-dom';
+
+//Layout
+import HelpLayout from './layouts/HelpLayout';
 import RootLayout from './layouts/RootLayout';
 
+//Pages
 import About from './pages/About';
 import Home from './pages/Home';
+import Faq from './pages/help/Faq';
+import Contact from './pages/help/Contact';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route path='/' element={<Home />} />
       <Route path='about' element={<About />} />
+      <Route path='help' element={<HelpLayout />}>
+        <Route path='faq' element={<Faq />} />
+        <Route path='contact' element={<Contact />} /> {/*help/contact */}
+      </Route>
     </Route>
   )
 );
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -8,6 +8,7 @@ const RootLayout = () => {
           <h1>PR-Router</h1>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='about'>About</NavLink>
+          <NavLink to='help'>Help</NavLink>
         </nav>
       </header>
       <main>
