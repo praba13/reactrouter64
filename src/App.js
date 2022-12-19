10f952d3a0b9035d772c3ff35e315e8680e3bdca
Praba <prama1303@gmail.com>
2022-12-19 22:57:24 +0100

    Page Note Found added with LINK

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Faq from './pages/help/Faq';
 import Contact from './pages/help/Contact';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
         <Route path='faq' element={<Faq />} />
         <Route path='contact' element={<Contact />} /> {/*help/contact */}
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Route>
   )
 );
